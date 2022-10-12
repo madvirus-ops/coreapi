23be1a420c79734809075a4281ecd049546c8454
madvirus-ops <edwinayabie1@gmail.com>
2022-10-12 05:48:18 +0100

    learning fastapi

---
+++ models.py
@@ -0,0 +1,17 @@
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    first_name:str
+    last_name:str
+    middle_name:Optional[str]
+    gender: Gender
